9e683d8f451bd5aa1bd95142a6a3b57cb7424bd0
abdulqadoos861 <abdulqadoosbhatti861@gmail.com>
2026-05-15 15:19:11 +0500

    Hey frontend is seprated backend . frontend is moved.

--- frontend/src/app/track-complaint/track-complaint.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-track-complaint',
-  imports: [],
-  templateUrl: './track-complaint.html',
-  styleUrl: './track-complaint.css',
-})
-export class TrackComplaint {}
